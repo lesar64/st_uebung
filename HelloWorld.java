d37f8e70c894e2426f43018350b546b81831af82
Jan F <jfiedle8@smail.uni-kolen.de>
2020-11-06 09:06:04 +0100

    Added Request

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
-public static void main(String[] args){
-    System.out.print('Hello World');
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Jan Fiedler");
+        System.out.println("Since 2016");
+    }
 }
